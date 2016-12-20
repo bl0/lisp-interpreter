a7e99a79f41e56337217fe99c218efe837851b4d
b-liu14 <b_liu14@163.com>
2016-12-20 23:02:20 +0800

    parse expr

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,12 +1,12 @@
-module Lib
-    (
-    someFunc,
-    eval
-    )where
+module Lib where
+    -- (
+    -- someFunc,
+    -- eval
+    -- )where
 
 -- So we don't pollute the global namespace
 import qualified Data.Map as Map
-
+import Data.Maybe
 -- We need to represent a variable name
 type Var = String
 
@@ -22,7 +22,7 @@ data Expr
 
     -- Reference the value of a variable. If the variable doesn't exist, it's an error
     | VarRef Var
-    deriving Show
+    deriving (Show, Read)
 
 data Stmt
 
@@ -40,7 +40,7 @@ data Stmt
 
     -- Skip out one level of "while" loop. It's an error if currently we are not in a loop
     | Skip
-    deriving Show
+    deriving (Show, Read)
 
 -- A program is a single statement
 type Prog = Stmt
@@ -50,16 +50,44 @@ data Val
 
     -- If you support other kinds of expressions, add the cases by yourself
     = BoolVal Bool
-    deriving Show
+    deriving (Show, Read, Eq)
 
 -- A memory is a mapping from variable names to values
 type Mem = Map.Map Var Val
 
+eval_expr :: Mem -> Expr -> Val
+eval_expr _ FalseLit = BoolVal False
+eval_expr _ TrueLit = BoolVal True
+eval_expr mem (Not expr)
+    | (v == (BoolVal False)) = BoolVal True
+    | otherwise = BoolVal False
+    where v = eval_expr mem expr
+eval_expr mem (And e1 e2)
+    | (v1 == BoolVal True && v2 == BoolVal True) = BoolVal True
+    | otherwise = BoolVal False
+    where
+        v1 = eval_expr mem e1
+        v2 = eval_expr mem e2
+eval_expr mem (Or e1 e2)
+    | v1 == BoolVal True || v2 == BoolVal True = BoolVal True
+    | otherwise = BoolVal False
+    where
+        v1 = eval_expr mem e1
+        v2 = eval_expr mem e2
+eval_expr mem (VarRef var) = fromMaybe (BoolVal False) $ Map.lookup var mem
+
 -- Evaluation function
 -- Given an initial memory, execute program and return the memory afterwards
 eval :: Prog -> Mem -> Mem
-eval = undefined
+eval Skip mem = mem
+-- eval (VarSet var expr) mem = let f _ = Just $ eval_expr expr in
 
+
+
+eval_helper :: String -> Mem -> Mem
+eval_helper s mem0 =
+    let prog = read s :: Prog in
+    eval prog mem0
 
 someFunc :: IO ()
 someFunc = putStrLn "HelloWo"
